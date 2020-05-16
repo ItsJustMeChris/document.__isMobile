00985996af0be83313375fe42d14bfe9e436aa64
Gerrit Alex <admin@gerritalex.de>
2020-05-16 16:17:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
 document.__isMobile = function() {
-  if( /iPhone|iPad|iPod|Android|IEMobile|webOS|BlackBerry|Opera Mini/i.test(navigator.userAgent) ) {
-    return true;
-  }
-  return false;
+  return /iPhone|iPad|iPod|Android|IEMobile|webOS|BlackBerry|Opera Mini/i.test(navigator.userAgent);
 }
